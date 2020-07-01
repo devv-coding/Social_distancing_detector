ec54f89c7f4c5d75c13d3050cfc60c277533468f
devv-coding <37217269+devv-coding@users.noreply.github.com>
2020-07-01 13:27:57 +0530

    Update SDD_Video+Webcam.py

--- SDD_Video+Webcam.py
+++ SDD_Video+Webcam.py
@@ -1,6 +1,3 @@
-# python SDD_Video+Webcam.py --input "rtsp://admin:WorldSpa@12345@worldspa.ddns.net:3556/Streaming/Channels/101?transportmode=unicast&profile=Profile_1"
-# python SDD_Video+Webcam.py --input output_test2.avi --output output4.mp4
-# python SDD_Video+Webcam.py --input output_test3_Trim.mp4
 import social_distance_cfg as config
 from detect import detect_people
 from scipy.spatial import distance as dist
@@ -41,11 +38,6 @@ ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
 print("[INFO] accessing video stream...")
 vs = cv2.VideoCapture(args["input"] if args["input"] else 0, cv2.CAP_FFMPEG)
-# vs.set(cv2.CAP_PROP_BUFFERSIZE, 3)
-# # cap.set(cv::CAP_PROP_BUFFERSIZE, 3)
-# vs.set(cv2.CAP_PROP_FPS, 9)
-violate = set()
-common = set()
 
 fps = vs.get(cv2.CAP_PROP_FPS)
 writer = None
@@ -62,28 +54,18 @@ while True:
 	results = detect_people(frame, net, ln,
 		personIdx=LABELS.index("person"))
 
-	# violate = set()
-	# common = set()
-	violate_current = set()
+	violate = set()
+	common = set()
 
 	if len(results) >= 2:
 		centroids = np.array([r[2] for r in results])
 		D = dist.cdist(centroids, centroids, metric="euclidean")
-		# cv2.line(frame, tuple(centroids), tuple(centroids), color=(0, 0, 0), thickness=3)
 
 		for i in range(0, D.shape[0]):
 			for j in range(i + 1, D.shape[1]):
 				if D[i, j] < config.MIN_DISTANCE:	
-					violate_current.add(i)# violate.add(j)
-					violate_current.add(j)# violate.add(j)														
-					common.add(i)
-					if len(violate.intersection(common)) == 0:
-						violate.add(i)
-					common.clear()					
-					common.add(j)
-					if len(violate.intersection(common)) == 0:
-						violate.add(j)
-					common.clear()						
+					violate.add(j)
+					violate.add(j)					
 					cv2.line(frame, tuple(centroids[i]), tuple(centroids[j]), color=(51, 51, 255), thickness=2)
 
 	for (i, (prob, bbox, centroid)) in enumerate(results):
@@ -92,11 +74,9 @@ while True:
 		color = (0, 255, 0)
 
 		if i in violate_current: color = (0, 0, 255); text = "Coming Close"; cv2.putText(frame, text, (startX, startY - 5), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 0, 255), 1); cv2.circle(frame, (cX, cY), 5, color, 1) 
-		# cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
-		# cv2.circle(frame, (cX, cY), 5, color, 2)
 
 	print("Frame ID : ",x)
-	y = len(violate) # + y
+	y = len(violate)
 	print("Social Distancing Violations: ",y)
 	text = "Social Distancing Violations: {} & FPS: {}".format(y,fps)
 	cv2.putText(frame, text, (10, frame.shape[0] - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.85, (0, 0, 255), 3), 
@@ -114,6 +94,5 @@ while True:
 	if writer is not None:
 		writer.write(frame)
 
-#writer.release() frame.shape[1], frame.shape[0]
 vs.release()
 cv2.destroyAllWindows()
